f45426e2f3a041e15289b73dd9c9df3750275c37
bruna_littlebodybigheart3 <bruna0952@hotmail.com>
2024-08-13 12:07:16 -0300

    apaguei o teste de erro

--- 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
+++ 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
@@ -1,4 +1,4 @@
-// TiposDeDados.cpp : This file contains the 'main' function. Program execution begins and ends there.bruna
+// TiposDeDados.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
